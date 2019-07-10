b2b8db80fe0dafb030872381c64a0ac523c0302d
Diego Garza <diego2500garza@uchicago.edu>
2019-07-10 17:19:17 -0500

    doneeeee

--- Assignment1/converter.py
+++ Assignment1/converter.py
@@ -2,9 +2,7 @@ print("This program converts from Fahrenheit to Celsius.")
 
 F = float(input("Enter temperature in degrees Fahrenheit:"))
 
-C = (9/5*F)+32 
-
-A = C
+C = ((F-32)*(5))/9
 
 for i in range(5):
-	print A, "degrees Celsius"
+	print C, "degrees Celsius"
