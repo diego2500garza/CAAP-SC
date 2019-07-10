e587599d916f128c3b902016d9be65f967316a62
Diego Alberto Garza <diego2500garza@tangelA.cs.uchicago.edu>
2019-07-10 16:57:42 -0500

    first 3 questions done

---
+++ Assignment1/converter.py
@@ -0,0 +1,10 @@
+print("This program converts from Fahrenheit to Celsius.")
+
+F = float(input("Enter temperature in degrees Fahrenheit:"))
+
+C = (9/5*F)+32 
+
+A = C, "degrees Celsius"
+
+for A in range(5):
+	print A
---
+++ Assignment1/unit_conversion.py
@@ -0,0 +1,8 @@
+print("This program converts from gallons to pints.")
+
+
+G = float(input("Enter number of gallons:"))
+
+P = 8*G
+
+print P, "pints"
