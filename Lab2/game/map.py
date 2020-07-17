89b0f97ffe13ec73d78f59264720ccdb7f163af1
diego2500garza <52261002+diego2500garza@users.noreply.github.com>
2020-07-17 11:00:27 -0500

    I hope this works

--- Lab2/game/map.py
+++ Lab2/game/map.py
@@ -2,48 +2,52 @@
 import scenes as S
 from death import Death
 
-# the map is created by the dictionary of scenes. If you add another parameter, 
+# the map is created by the dictionary of scenes. If you add another parameter,
 # you can probably add your own custom maps (as long as they somehow lead to end)?
+
+
 class Map(object):
-	#key is strings, value is function
-	scenes = {'central_corridor' : S.CentralCorridor(), #class names bc they're capitalized. calling class name __init__. String keys are assigned to the scene object. They're scene objects because () calling the constructorn function inside class
-				'laser_weapon_armory' : S.LaserWeaponArmory(),
-				'guards' : S.Guards(),
-				'foot_steps': S.Foot_Steps(),
-				#Hallway Path
-				'hallway': S.Hallway(),
-				'infirmary' : S.Infirmary(),
-				'storage': S.Storage(),
-			
-				'box' : S.Box(),
-				'escape' : S.Escape(),
+    #key is strings, value is function
+    scenes = {'central_corridor': S.CentralCorridor(),  # class names bc they're capitalized. calling class name __init__. String keys are assigned to the scene object. They're scene objects because () calling the constructorn function inside class
+              'laser_weapon_armory': S.LaserWeaponArmory(),
+              'guards': S.Guards(),
+              'foot_steps': S.Foot_Steps(),
+              # Hallway Path
+              'hallway': S.Hallway(),
+              'infirmary': S.Infirmary(),
+              'storage': S.Storage(),
 
-				'ask' : S.Ask(),
-				'boarding' : S.Boarding(),
-				'incoming' : S.Incoming(),
-				
-				#Others Path
-				'others' : S.Others(),
-				'pub_Entrance' : S.Pub(),
-				'poker' : S.Poker(),
-				'winnah' : S.Winnah(),
+              'box': S.Box(),
+              'escape': S.Escape(),
 
-				#Exit
-				'exit' : S.Exit(),
-				#Death
-				'death' : Death()
-				# raise ValueError ('todo')
-				}
-	
-	# initializes to a starting scene
-	def __init__(self, start_scene): #this is the constructor
-		self.start_scene = start_scene
-	
-	# gets the specified scene from the scenes dictionary list.
-	def next_scene(self, scene_name):
-		return self.scenes.get(scene_name) #sending in scene_name, self.start_scene is assigned as scene_name SAME VALUE THO
-		#self.scenes in this line gets
-	
-	# gets the first scene of the map from the dictionary list
-	def opening_scene(self):
-		return self.next_scene(self.start_scene) # Dominos calling Papa Johns, start_scene is order
+              'ask': S.Ask(),
+              'boarding': S.Boarding(),
+              'incoming': S.Incoming(),
+
+              # Others Path
+              'others': S.Others(),
+              'pub_Entrance': S.Pub(),
+              'poker': S.Poker(),
+              'winnah': S.Winnah(),
+
+              # Exit
+              'exit': S.Exit(),
+              # Death
+              'death': Death()
+              # raise ValueError ('todo')
+              }
+
+    # initializes to a starting scene
+    def __init__(self, start_scene):  # this is the constructor
+        self.start_scene = start_scene
+
+    # gets the specified scene from the scenes dictionary list.
+    def next_scene(self, scene_name):
+        # sending in scene_name, self.start_scene is assigned as scene_name SAME VALUE THO
+        return self.scenes.get(scene_name)
+        # self.scenes in this line gets
+
+    # gets the first scene of the map from the dictionary list
+    def opening_scene(self):
+        # Dominos calling Papa Johns, start_scene is order
+        return self.next_scene(self.start_scene)
---
+++ Lab2/game/text.py
@@ -0,0 +1,8 @@
+class Textando:
+
+    def texting(self):
+        print("this is the text")
+
+
+sample_text = Textando()
+sample_text.texting()
